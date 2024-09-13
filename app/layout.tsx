78c9805a18efdf843bac2f266984395cf024079a
Muskan Fatima <160476952+muskan-fatim@users.noreply.github.com>
2024-09-13 00:22:27 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,29 @@
-import type { Metadata } from "next";
+
+ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+// Define the geistSans font
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
+// Define the geistMono font
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
+// Metadata for the page
 export const metadata: Metadata = {
-  title: "Resume Builder ",
-  description: "resume builder with all the basic functinalty",
+  title: "Resume Builder",
+  description: "Resume builder with all the basic functionality",
 };
 
+// RootLayout component
 export default function RootLayout({
   children,
 }: Readonly<{
